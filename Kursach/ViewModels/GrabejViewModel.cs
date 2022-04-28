b30888bc37d04ecbecf641485456732e78ee3b5a
user <user@WIN-962LDTIVMTK>
2022-04-28 19:13:30 +1000

    Добавьте файлы проекта.

---
+++ Kursach/ViewModels/GrabejViewModel.cs
@@ -0,0 +1,117 @@
+﻿using Kursach.Objects;
+using System;
+
+namespace Kursach.ViewModels;
+
+internal class GrabejViewModel : ViewModelBase
+{
+    public GrabejViewModel()
+    {
+        PayCommand = new(o =>
+        {
+            MainViewModel.Instance.Time = TimeSpan.FromHours(SelectedTarif.Time);
+            MainViewModel.Instance.Timer.Start();
+        });
+    }
+    private double _hours;
+    public double Hours
+    {
+        get => _hours;
+        set
+        {
+            _hours = value;
+            Signal();
+
+            if (SelectedTarif is not null)
+                Price = SelectedTarif.Price;
+        }
+    }
+
+    protected double _price;
+    public double Price
+    {
+        get => _price;
+        set
+        {
+            _price = value;
+            Signal();
+        }
+    }
+
+    private Tarif _selectedTarif;
+    public Tarif SelectedTarif
+    {
+        get => _selectedTarif;
+        set
+        {
+            _selectedTarif = value;
+            if (value.Title == "Обычный")
+                Hours = 1;
+            if (value.Title == "3 Часа")
+                Hours = 3;
+            if (value.Title == "Дотерский")
+                Hours = 5; 
+            if (value.Title == "10 Часов")
+                Hours = 10;
+            if (value.Title == "Киберспортсмен")
+                Hours = 12;
+            if (value.Title == "Сутки напролёт")
+                Hours = 24;
+
+            Signal();
+
+        }
+    }
+
+    public Tarif[] Tarify { get; set; } = new Tarif[]
+    {
+        new()
+        {
+            Title = "Обычный",
+           Price = 60,
+           Time = 1
+        },
+        new()
+        {
+            Title = "3 Часа",
+            Price = 150,
+            Time = 3
+        },
+         new()
+         {
+            Title = "Дотерский",
+           Price = 250,
+           Time = 5
+         },
+
+        new()
+        {
+         Title = "10 Часов",
+         Price = 550,
+         Time = 10
+        },
+
+        new()
+        {
+         Title = "Киберспортсмен",
+         Price = 650,
+         Time = 12
+        },
+
+        new()
+        {
+            Title = "Сутки напролёт",
+           Price = 1200,
+           Time = 24
+        }
+    };
+
+    public Command PayCommand { get; init; } 
+}
+
+public class Tarif
+{
+    public string Title { get; set; }
+    public double Price { get; set; }
+    public int Time { get; set; }
+}
---
+++ Kursach/ViewModels/MainViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Kursach.Objects;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Kursach.ViewModels
+{
+    internal class MainViewModel : ViewModelBase
+    {
+    
+        public MainViewModel()
+        {
+            Timer.Tick += Timer_Tick;
+            Timer.Interval = new(0, 1, 0);
+        }
+
+        public static MainViewModel Instance { get; } = new();
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            Time.Add(TimeSpan.FromMinutes(-1));
+
+            if (Time == TimeSpan.Zero)
+                Timer.Stop();            
+        }
+
+        public DispatcherTimer Timer { get; } = new();
+        private TimeSpan _time;
+        public TimeSpan Time
+        {
+            get => _time;
+            set
+            {
+                _time = value;
+                Signal();
+            }
+        }
+        public Command ExitCommand { get; } = new(o => Application.Current.Shutdown());
+        public Command HideCommand { get; } = new(o => Application.Current.MainWindow.WindowState = WindowState.Minimized);
+
+        public Command TelegaCommand { get; } = new(o =>
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                UseShellExecute = true,
+                FileName = "http://t.me/Night_bred"
+            });
+        });
+    }
+}
+
+/*
+ * t.me/Night_bred
+ */
